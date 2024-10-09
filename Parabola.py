41c0d4dc88d183e7f70c765aada2e1aff53058e8
Guif53 <gumartin@myuax.com>
2024-10-09 11:40:30 +0200

    Update Parabola.py

--- Parabola.py
+++ Parabola.py
@@ -1,2 +1,10 @@
-import matplotlib as mat
+import matplotlib.pyplot as plt
 
+x = np.linspace()
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.title("Grafico de la funcion seno")
+plt.xlabel("Eje x")
+plt.ylabel("Eje y")
+plt.show() asfdvliadbvlabdflv
